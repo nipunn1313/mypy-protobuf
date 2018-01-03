ce58d1d5d283638f8445c6e8658240a41b75666a
Nipunn Koorapati <nipunn@dropbox.com>
2018-01-03 14:46:26 -0800

    Add python generation script

---
+++ python/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='mypy-protobuf',
+    version='1.0',
+    description='Generate mypy stub files from protobuf specs',
+    keywords='mypy proto dropbox',
+    license='Apache License 2.0',
+    author='Nipunn Koorapati',
+    author_email='nipunn@dropbox.com',
+    py_modules=[],
+    url='https://github.com/dropbox/mypy-protobuf',
+    download_url='https://github.com/dropbox/mypy-protobuf/tarball/1.0',
+
+    scripts=['generate_proto_mypy_stubs.py', 'generate_proto_mypy_stubs.bat'],
+)
