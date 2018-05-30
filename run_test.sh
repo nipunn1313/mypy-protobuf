8c8a030163bccae48381ef705d32619eedeca9bc
Nipunn Koorapati <nipunn1313@gmail.com>
2018-05-29 23:45:36 -0700

    Set up virtualenv in CI

--- run_test.sh
+++ run_test.sh
@@ -1,13 +1,30 @@
 #!/bin/bash -ex
 
-# Install requirements
-python -m pip install -r requirements.txt
+(
+    # Create virtualenv + Install requirements for mypy-protobuf
+    python -m virtualenv env
+    source env/bin/activate
+    python -m pip install -r requirements.txt
 
-# Generate protos
-protoc --mypy_out=test/output/ --plugin=protoc-gen-mypy=python/protoc-gen-mypy --proto_path=test/proto/ `find test/proto -name "*.proto"`
+    # Generate protos
+    protoc --python_out=test/output/ --mypy_out=test/output/ --plugin=protoc-gen-mypy=python/protoc-gen-mypy --proto_path=test/proto/ `find test/proto -name "*.proto"`
 
-# Run unit tests
-(cd test ; py.test)
+    # Run unit tests
+    cd test
+    py.test
+)
 
-# Run mypy
-mypy --python-version=2.7 python/protoc-gen-mypy test/
+(
+    # Uncomment these to run on mac
+    #eval "$(pyenv init -)"
+    #pyenv shell 3.5.5
+
+    # Create virtualenv
+    python3 -m virtualenv mypy_env
+    source mypy_env/bin/activate
+    python3 -m pip install -U setuptools
+    python3 -m pip install -U git+git://github.com/python/mypy.git
+
+    # Run mypy
+    mypy --python-version=$PY python/protoc-gen-mypy test/
+)
