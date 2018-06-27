d48d67ab5b8825b71de93596a8202e4a4668cab5
Nipunn Koorapati <nipunn@dropbox.com>
2018-06-27 15:28:14 -0700

    Bump to version 1.6

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mypy-protobuf',
-    version='1.5',
+    version='1.6',
     description='Generate mypy stub files from protobuf specs',
     keywords='mypy proto dropbox',
     license='Apache License 2.0',
@@ -10,7 +10,7 @@ setup(
     author_email='nipunn@dropbox.com',
     py_modules=[],
     url='https://github.com/dropbox/mypy-protobuf',
-    download_url='https://github.com/dropbox/mypy-protobuf/tarball/1.5',
+    download_url='https://github.com/dropbox/mypy-protobuf/tarball/1.6',
 
     scripts=['protoc-gen-mypy', 'protoc_gen_mypy.bat'],
 )
