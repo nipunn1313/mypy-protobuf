e951d1822e8d6c8a30e49d5a790b40315ca5e290
Nipunn Koorapati <nipunn@dropbox.com>
2019-07-22 11:15:58 -0700

    Bump to 1.14

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mypy-protobuf',
-    version='1.13',
+    version='1.14',
     description='Generate mypy stub files from protobuf specs',
     keywords='mypy proto dropbox',
     license='Apache License 2.0',
@@ -10,7 +10,7 @@ setup(
     author_email='nipunn@dropbox.com',
     py_modules=[],
     url='https://github.com/dropbox/mypy-protobuf',
-    download_url='https://github.com/dropbox/mypy-protobuf/archive/v1.13.tar.gz',
+    download_url='https://github.com/dropbox/mypy-protobuf/archive/v1.14.tar.gz',
     install_requires=['protobuf>=3.6.0'],
 
     scripts=['protoc-gen-mypy', 'protoc_gen_mypy.bat'],
