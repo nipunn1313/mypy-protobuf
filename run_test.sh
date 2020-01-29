b072fc9c89aa9f776c4cfb97fc7f33b04a8cc101
Mikołaj Zalewski <mikolajz@nomagic.ai>
2020-01-29 21:15:02 +0100

    Bump mypy version - Python2 tests now start failing.

--- run_test.sh
+++ run_test.sh
@@ -28,7 +28,7 @@ MYPY_VENV=venv_mypy
     source $MYPY_VENV/bin/activate
     if [[ -z $SKIP_CLEAN ]] || [[ ! -e $MYPY_VENV ]]; then
         python3 -m pip install setuptools
-        python3 -m pip install git+https://github.com/python/mypy.git@v0.711
+        python3 -m pip install git+https://github.com/python/mypy.git@v0.761
     fi
 
     # Run mypy
