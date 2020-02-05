d5ec0aaed8041847688f514924ddaa8d46480d68
Chad Dombrova <chadrik@gmail.com>
2020-02-04 23:03:26 -0800

    Use entry_points:console_scripts to generate protoc-gen-mypy
    This fixes an issue where the shebang line fails of the executable fails to work when the path to the python interpreter exceeds 127 characters on linux and 512 on darwin.

--- python/setup.py
+++ python/setup.py
@@ -8,10 +8,14 @@ setup(
     license='Apache License 2.0',
     author='Nipunn Koorapati',
     author_email='nipunn@dropbox.com',
-    py_modules=[],
+    py_modules=['mypy_protobuf'],
     url='https://github.com/dropbox/mypy-protobuf',
     download_url='https://github.com/dropbox/mypy-protobuf/archive/v1.17.tar.gz',
     install_requires=['protobuf>=3.6.0'],
-
-    scripts=['protoc-gen-mypy', 'protoc_gen_mypy.bat'],
+    entry_points={
+        "console_scripts": [
+            'protoc-gen-mypy = mypy_protobuf:main'
+        ]
+    },
+    scripts=['protoc_gen_mypy.bat'],
 )
