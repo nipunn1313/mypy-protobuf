781072cd115b9b2a9e1b3575f32775a59ce40858
Eugene Chernyshov <eugene@thefloorcyber.com>
2020-12-27 16:22:18 +0200

    shield grpc tests with python version

--- run_test.sh
+++ run_test.sh
@@ -7,6 +7,7 @@ MYPY_VENV=venv_mypy
 RED="\033[0;31m"
 NC='\033[0m'
 PROTOC=${PROTOC:=protoc}
+CAN_TEST_GRPC_OUTPUT=`python -c 'import sys;print(sys.version_info.major>=3 and sys.version_info.minor>=6)'`
 
 # Clean out generated/ directory - except for .generated / __init__.py
 find generated -type f -not \( -name "*.expected" -or -name "__init__.py" \) -delete
@@ -29,7 +30,10 @@ find generated -type f -not \( -name "*.expected" -or -name "__init__.py" \) -de
     fi
     $PROTOC --mypy_out=generated --proto_path=proto/ --experimental_allow_proto3_optional `find proto/ -name "*.proto"`
     $PROTOC --python_out=generated --proto_path=proto/ --experimental_allow_proto3_optional `find proto/testproto -name "*.proto"`
-    $PROTOC --mypy_grpc_out=generated --proto_path=proto/ --experimental_allow_proto3_optional `find proto/testproto/grpc -name "*.proto"`
+
+    if [[ $CAN_TEST_GRPC_OUTPUT = "True" ]]; then
+        $PROTOC --mypy_grpc_out=generated --proto_path=proto/ --experimental_allow_proto3_optional `find proto/testproto/grpc -name "*.proto"`
+    fi
 )
 
 (
@@ -44,7 +48,9 @@ find generated -type f -not \( -name "*.expected" -or -name "__init__.py" \) -de
     source $MYPY_VENV/bin/activate
     if [[ -z $SKIP_CLEAN ]] || [[ ! -e $MYPY_VENV ]]; then
         python3 -m pip install setuptools
-        python3 -m pip install git+https://github.com/Evgenus/grpc-stubs.git@76aab756c87716566835600a6683710264890950
+        if [[ $CAN_TEST_GRPC_OUTPUT = "True" ]]; then
+            python3 -m pip install grpc-stubs
+        fi
         python3 -m pip install git+https://github.com/python/mypy.git@985a20d87eb3a516ff4457041a77026b4c6bd784
     fi
 
