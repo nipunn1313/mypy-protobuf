c7528c456448958f43a6f8bdd503be505d0e63b6
Nipunn Koorapati <nipunn1313@gmail.com>
2021-07-22 14:49:44 -0500

    Run both py2 and py3 unit tests on each run of run_test.sh
    Was running into issues w/ generating negative test results
    without having the 2.7 venv available. This should achieve it.
    Should also simplify the main.yml

--- run_test.sh
+++ run_test.sh
@@ -6,10 +6,8 @@ PROTOC=${PROTOC:=protoc}
 
 PY_VER_MYPY_PROTOBUF=${PY_VER_MYPY_PROTOBUF:=3.9.6}
 PY_VER_MYPY_PROTOBUF_SHORT=$(echo $PY_VER_MYPY_PROTOBUF | cut -d. -f1-2)
-
 PY_VER_MYPY=${PY_VER_MYPY:=3.8.11}
-PY_VER_MYPY_TARGET=${PY_VER_MYPY_TARGET:=3.8}
-PY_VER_UNIT_TESTS=${PY_VER_UNIT_TESTS:=3.8.11}
+PY_VER_UNIT_TESTS="${PY_VER_UNIT_TESTS_3:=3.8.11} ${PY_VER_UNIT_TESTS_2:=2.7.18}"
 
 # Clean out generated/ directory - except for .generated / __init__.py
 find test/generated -type f -not \( -name "*.expected" -or -name "__init__.py" \) -delete
@@ -35,20 +33,22 @@ MYPY_VENV=venv_$PY_VER_MYPY
     $MYPY_VENV/bin/mypy --version
 )
 
-# Create unit tests venv
-UNIT_TESTS_VENV=venv_$PY_VER_UNIT_TESTS
-(
-    eval "$(pyenv init --path)"
-    eval "$(pyenv init -)"
-    pyenv shell $PY_VER_UNIT_TESTS
+# Create unit tests venvs
+for PY_VER in $PY_VER_UNIT_TESTS; do
+    (
+        UNIT_TESTS_VENV=venv_$PY_VER
+        eval "$(pyenv init --path)"
+        eval "$(pyenv init -)"
+        pyenv shell $PY_VER
 
-    if [[ -z $SKIP_CLEAN ]] || [[ ! -e $UNIT_TESTS_VENV ]]; then
-        python -m pip install virtualenv
-        python -m virtualenv $UNIT_TESTS_VENV
-        $UNIT_TESTS_VENV/bin/python -m pip install -r test_requirements.txt
-    fi
-    $UNIT_TESTS_VENV/bin/py.test --version
-)
+        if [[ -z $SKIP_CLEAN ]] || [[ ! -e $UNIT_TESTS_VENV ]]; then
+            python -m pip install virtualenv
+            python -m virtualenv $UNIT_TESTS_VENV
+            $UNIT_TESTS_VENV/bin/python -m pip install -r test_requirements.txt
+        fi
+        $UNIT_TESTS_VENV/bin/py.test --version
+    )
+done
 
 # Create mypy-protobuf venv
 MYPY_PROTOBUF_VENV=venv_$PY_VER_MYPY_PROTOBUF
@@ -108,56 +108,62 @@ MYPY_PROTOBUF_VENV=venv_$PY_VER_MYPY_PROTOBUF
     python -m grpc_tools.protoc $PROTOC_ARGS --grpc_python_out=test/generated $GRPC_PROTOS
 )
 
-# Run mypy on unit tests / generated output
-(
-    source $MYPY_VENV/bin/activate
+for PY_VER in $PY_VER_UNIT_TESTS; do
+    UNIT_TESTS_VENV=venv_$PY_VER
+    PY_VER_MYPY_TARGET=$(echo $PY_VER | cut -d. -f1-2)
 
-    # Run mypy
-    # --python-version=2.7 chokes on the generated grpc files - so split them out here
-    FILES27="$(ls test/*.py | grep -v grpc)  $(find test/generated -name "*.pyi" | grep -v grpc)"
-    FILES38="test/"
-    if [ $PY_VER_MYPY_TARGET = "2.7" ]; then
-        FILES=$FILES27
-    else
-        FILES=$FILES38
-    fi
-    mypy --strict --custom-typeshed-dir=$CUSTOM_TYPESHED_DIR --python-executable=$UNIT_TESTS_VENV/bin/python --python-version=$PY_VER_MYPY_TARGET --pretty --show-error-codes $FILES
+    # Run mypy on unit tests / generated output
+    (
+        source $MYPY_VENV/bin/activate
 
-    # run mypy on negative-tests (expected mypy failures)
-    # --python-version=2.7 chokes on the generated grpc files - so split them out here
-    NEGATIVE_FILES_27="test_negative/negative.py test_negative/negative_2.7.py $FILES27"
-    NEGATIVE_FILES_38="test_negative/negative.py test_negative/negative_3.8.py $FILES38"
-    if [ $PY_VER_MYPY_TARGET = "2.7" ]; then
-        NEGATIVE_FILES=$NEGATIVE_FILES_27
-    else
-        NEGATIVE_FILES=$NEGATIVE_FILES_38
-    fi
+        # Run mypy
+        # --python-version=2.7 chokes on the generated grpc files - so split them out here
+        FILES27="$(ls test/*.py | grep -v grpc)  $(find test/generated -name "*.pyi" | grep -v grpc)"
+        FILES38="test/"
+        if [ $PY_VER_MYPY_TARGET = "2.7" ]; then
+            FILES=$FILES27
+        else
+            FILES=$FILES38
+        fi
+        mypy --strict --custom-typeshed-dir=$CUSTOM_TYPESHED_DIR --python-executable=$UNIT_TESTS_VENV/bin/python --python-version=$PY_VER_MYPY_TARGET --pretty --show-error-codes $FILES
 
-    MYPY_OUTPUT=`mktemp -d`
-    call_mypy() {
-        # Write output to file. Make variant w/ omitted line numbers for easy diffing / CR
-        mypy --strict --custom-typeshed-dir=$CUSTOM_TYPESHED_DIR --python-executable=$UNIT_TESTS_VENV/bin/python --python-version=$1 ${@: 2} > $MYPY_OUTPUT/mypy_output || true
-        cut -d: -f1,3- $MYPY_OUTPUT/mypy_output > $MYPY_OUTPUT/mypy_output.omit_linenos
-    }
+        # run mypy on negative-tests (expected mypy failures)
+        # --python-version=2.7 chokes on the generated grpc files - so split them out here
+        NEGATIVE_FILES_27="test_negative/negative.py test_negative/negative_2.7.py $FILES27"
+        NEGATIVE_FILES_38="test_negative/negative.py test_negative/negative_3.8.py $FILES38"
+        if [ $PY_VER_MYPY_TARGET = "2.7" ]; then
+            NEGATIVE_FILES=$NEGATIVE_FILES_27
+        else
+            NEGATIVE_FILES=$NEGATIVE_FILES_38
+        fi
 
-    call_mypy $PY_VER_MYPY_TARGET $NEGATIVE_FILES
-    if ! diff $MYPY_OUTPUT/mypy_output test_negative/output.expected.$PY_VER_MYPY_TARGET || ! diff $MYPY_OUTPUT/mypy_output.omit_linenos test_negative/output.expected.$PY_VER_MYPY_TARGET.omit_linenos; then
-        echo -e "${RED}test_negative/output.expected.$PY_VER_MYPY_TARGET didnt match. Copying over for you. Now rerun${NC}"
+        MYPY_OUTPUT=`mktemp -d`
+        call_mypy() {
+            # Write output to file. Make variant w/ omitted line numbers for easy diffing / CR
+            PY_VER_MYPY_TARGET=$(echo $1 | cut -d. -f1-2)
+            mypy --strict --custom-typeshed-dir=$CUSTOM_TYPESHED_DIR --python-executable=venv_$1/bin/python --python-version=$PY_VER_MYPY_TARGET ${@: 2} > $MYPY_OUTPUT/mypy_output || true
+            cut -d: -f1,3- $MYPY_OUTPUT/mypy_output > $MYPY_OUTPUT/mypy_output.omit_linenos
+        }
 
-        # Copy over all the mypy results for the developer.
-        call_mypy 2.7 $NEGATIVE_FILES_27
-        cp $MYPY_OUTPUT/mypy_output test_negative/output.expected.2.7
-        cp $MYPY_OUTPUT/mypy_output.omit_linenos test_negative/output.expected.2.7.omit_linenos
-        call_mypy 3.8 $NEGATIVE_FILES_38
-        cp $MYPY_OUTPUT/mypy_output test_negative/output.expected.3.8
-        cp $MYPY_OUTPUT/mypy_output.omit_linenos test_negative/output.expected.3.8.omit_linenos
-        exit 1
-    fi
-)
+        call_mypy $PY_VER $NEGATIVE_FILES
+        if ! diff $MYPY_OUTPUT/mypy_output test_negative/output.expected.$PY_VER_MYPY_TARGET || ! diff $MYPY_OUTPUT/mypy_output.omit_linenos test_negative/output.expected.$PY_VER_MYPY_TARGET.omit_linenos; then
+            echo -e "${RED}test_negative/output.expected.$PY_VER_MYPY_TARGET didnt match. Copying over for you. Now rerun${NC}"
 
-(
-    # Run unit tests. These tests generate .expected files
-    source $UNIT_TESTS_VENV/bin/activate
-    if [[ $PY_VER_UNIT_TESTS =~ ^2.* ]]; then IGNORE="--ignore=test/test_grpc_usage.py"; else IGNORE=""; fi
-    PYTHONPATH=test/generated py.test --ignore=test/generated $IGNORE -v
-)
+            # Copy over all the mypy results for the developer.
+            call_mypy 2.7.18 $NEGATIVE_FILES_27
+            cp $MYPY_OUTPUT/mypy_output test_negative/output.expected.2.7
+            cp $MYPY_OUTPUT/mypy_output.omit_linenos test_negative/output.expected.2.7.omit_linenos
+            call_mypy 3.8.11 $NEGATIVE_FILES_38
+            cp $MYPY_OUTPUT/mypy_output test_negative/output.expected.3.8
+            cp $MYPY_OUTPUT/mypy_output.omit_linenos test_negative/output.expected.3.8.omit_linenos
+            exit 1
+        fi
+    )
+
+    (
+        # Run unit tests. These tests generate .expected files
+        source $UNIT_TESTS_VENV/bin/activate
+        if [[ $PY_VER =~ ^2.* ]]; then IGNORE="--ignore=test/test_grpc_usage.py"; else IGNORE=""; fi
+        PYTHONPATH=test/generated py.test --ignore=test/generated $IGNORE -v
+    )
+done
