2d9e796f6107c7ca5b58cc09787fc57a84332bcf
Jack Strang <jack@tutorintelligence.com>
2021-08-16 17:23:23 -0400

    assert false

--- mypy_protobuf/main.py
+++ mypy_protobuf/main.py
@@ -739,6 +739,7 @@ class PkgWriter(object):
         services: Iterable[d.ServiceDescriptorProto],
         scl_prefix: SourceCodeLocation,
     ) -> None:
+        assert False
         l = self._write_line
         l(
             "from .{} import *",
